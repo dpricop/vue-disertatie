3eecabc2e14d890457d24889261940920c0b44fb
dpricop <pricopdumitru94@gmail.com>
2021-05-22 03:00:32 +0300

    Axios, VueAxios has been added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import 'bootstrap/dist/js/bootstrap.min'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import "bootstrap/dist/js/bootstrap.min";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueAxios, axios)
+  .mount("#app");
