d11203a2f4f8b608f3ffe1f49cb1cf287930d71e
dpricop <pricopdumitru94@gmail.com>
2021-05-31 23:33:16 +0300

    LeadStatus, OpportunityFaza, OpportunityStatus CRUD - has been added

---
+++ src/components/Crm/CrmOpportunityStatus/CrmOpportunityStatusForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="modal fade"
+    data-bs-backdrop="static"
+    id="CrmOpportunityStatusForm"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Edit opportunity status</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="new-item" class="col-form-label">Status:</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="dataSource.StatusNume"
+              />
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-outline-danger"
+            data-bs-dismiss="modal"
+          >
+            <i class="cil-x-circle"></i>
+            <span class="mx-2">Cancel</span>
+          </button>
+          <button type="button" class="btn btn-success" @click="editedItem">
+            <i class="cil-save"></i>
+            <span class="mx-2">Save</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CrmOpportunityStatusForm",
+  props: {
+    propsData: {
+      IdOpportunityStatus: Number,
+      StatusNume: String,
+    },
+  },
+  data: function(e) {
+    return {
+      dataSource: this.propsData,
+    };
+  },
+  methods: {
+    editedItem() {
+      this.$emit("editedItem", this.dataSource);
+    },
+  },
+};
+</script>
