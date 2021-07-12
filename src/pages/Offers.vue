1b3c26e4d7998d916768653d733f11cb1ddbf488
dpricop <pricopdumitru94@gmail.com>
2021-07-13 01:04:10 +0300

    Offers page - loading has been removed

--- src/pages/Offers.vue
+++ src/pages/Offers.vue
@@ -1,25 +1,18 @@
 <template>
-  <template v-if="isLoading == true">
-    <loading />
-  </template>
-  <template v-else>
-    <div class="col-12 p-3"><h1>Offers</h1></div>
+  <div class="col-12 p-3"><h1>Offers</h1></div>
 
-    <div class="col-12 col-md-10 col-lg-8 py-3 bg-light">
-      <div class="table-responsive">
-        <crm-offers-header-table />
-      </div>
+  <div class="col-12 col-md-10 col-lg-8 py-3 bg-light">
+    <div class="table-responsive">
+      <crm-offers-header-table />
     </div>
-  </template>
+  </div>
 </template>
 <script>
-import Loading from "@/components/Loading";
 import CrmOffersHeaderTable from "../components/Crm/CrmOffers/CrmOffersHeaderTable.vue";
 
 export default {
   name: "offers",
   components: {
-    Loading,
     CrmOffersHeaderTable,
   },
 };
