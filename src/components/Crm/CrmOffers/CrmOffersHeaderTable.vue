1e248a3da53b4ffcbb593e46828621318c63d285
dpricop <pricopdumitru94@gmail.com>
2021-07-13 00:26:26 +0300

    Offers page ,  CrmOffersHeaderTable Component - has been added

---
+++ src/components/Crm/CrmOffers/CrmOffersHeaderTable.vue
@@ -0,0 +1,218 @@
+<template>
+  <template v-if="isLoading == true">
+    <loading />
+  </template>
+  <template v-else>
+    <table class="table table-bordered bg-white mb-0">
+      <thead>
+        <tr>
+          <th colspan="12" class="text-end">
+            <button
+              class="btn btn-sm btn-success px-3"
+              data-bs-toggle="modal"
+              data-bs-target="#CrmParteneriForm"
+              @Click="addItemBtn"
+            >
+              <i class="cil-plus"></i>
+              <span>Add</span>
+            </button>
+          </th>
+        </tr>
+        <tr>
+          <th>#</th>
+          <th>Partner</th>
+          <th>Expiration date</th>
+          <th>Date of issue</th>
+          <th>Is accepted</th>
+          <th>Is rejected</th>
+          <th>Has opportunity</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) of dataSource" :key="item.IdOfertaAntent">
+          <td>{{ index + 1 }}</td>
+          <td>
+            <router-link
+              class="text-decoration-none"
+              :to="{
+                name: 'partner',
+                params: { id: item.PartenerId },
+              }"
+              target="_blank"
+            >
+              {{ item.NumePartener }}
+            </router-link>
+          </td>
+          <td>{{ $filters.dateTimeFormat(item.DataEmiterii) }}</td>
+          <td>{{ $filters.dateTimeFormat(item.DataExpirarii) }}</td>
+
+          <td class="text-center">
+            <i
+              class="cil-check-circle text-success"
+              v-if="item.OfertaAcceptata == true"
+            ></i>
+            <i
+              class="cil-x-circle text-danger"
+              v-if="item.OfertaAcceptata == false"
+            ></i>
+          </td>
+          <td class="text-center">
+            <i
+              class="cil-check-circle text-success"
+              v-if="item.OfertaRespinsa == true"
+            ></i>
+            <i
+              class="cil-x-circle text-danger"
+              v-if="item.OfertaRespinsa == false"
+            ></i>
+          </td>
+          <td class="text-center">
+            <i
+              class="cil-check-circle text-success"
+              v-if="item.OpportunityId != null"
+            ></i>
+            <i class="cil-x-circle text-danger"></i>
+          </td>
+          <td>
+            <router-link
+              class="text-decoration-none text-success m-2"
+              :to="{
+                name: 'offer',
+                params: { id: item.IdOfertaAntent },
+              }"
+              target="_blank"
+            >
+              <i class="cil-chevron-circle-right-alt"></i>
+            </router-link>
+            <a
+              class="text-decoration-none text-warning m-2"
+              @click.prevent="editItemBtn(item)"
+              href="#"
+              data-bs-toggle="modal"
+              data-bs-target="#CrmParteneriForm"
+            >
+              <i class="cil-pencil"></i>
+            </a>
+            <a
+              class="text-decoration-none text-danger m-2"
+              @click.prevent="deleteItem(item, index)"
+              href="#"
+            >
+              <i class="cil-trash"></i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- <crm-parteneri-form
+      v-bind:propsData="editItemData"
+      :key="editItemData"
+      @editedItem="editedItem"
+    /> -->
+  </template>
+</template>
+<script>
+import Loading from "@/components/Loading";
+// import CrmParteneriForm from "./CrmParteneriForm";
+
+export default {
+  name: "CrmOffersHeaderTable",
+  components: {
+    // CrmParteneriForm,
+    Loading,
+  },
+  data: function(e) {
+    return {
+      isLoading: true,
+      newItem: "",
+      editItemData: {},
+      dataSource: [],
+    };
+  },
+  mounted() {
+    this.reloadStore();
+  },
+  computed: {},
+  methods: {
+    async reloadStore() {
+      await this.$store.dispatch("offers/getDataSource");
+      this.getData();
+    },
+    getData() {
+      if (this.vuexGetter == undefined) {
+        this.dataSource = this.$store.getters["offers/getAll"];
+      } else {
+        if (this.vuexParam == undefined) {
+          this.dataSource = this.$store.getters["offers/" + this.vuexGetter];
+        } else {
+          this.dataSource = this.$store.getters["offers/" + this.vuexGetter](
+            this.vuexParam
+          );
+        }
+      }
+      if (!Array.isArray(this.dataSource)) {
+        this.dataSource = [this.dataSource];
+      }
+      this.isLoading = false;
+    },
+
+    addItemBtn() {
+      this.editItemData.isEditing = false;
+    },
+    editItemBtn(item) {
+      this.editItemData.isEditing = true;
+    },
+    formModalToggle() {
+      // var formModal = window.bootstrap.Modal.getInstance(
+      //   document.getElementById("CrmParteneriForm")
+      // );
+      // formModal.toggle();
+
+      return true;
+    },
+    editedItem(params) {
+      params.isEditing == true ? this.editItem(params) : this.addItem(params);
+    },
+    addItem(params) {
+      const data = new FormData();
+      data.append("values", JSON.stringify(params));
+      this.axios
+        .post("http://localhost:8080/CrmOferteAntent/Create", data)
+        .then((response) => {
+          if (response.data.status == 1 && response.data.count == 1) {
+            this.reloadStore();
+            this.formModalToggle();
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+    editItem(params) {
+      const data = new FormData();
+      data.set("id", params.IdOfertaAntent);
+      data.set("values", JSON.stringify(params));
+      this.axios
+        .put("http://localhost:8080/CrmOferteAntent/Edit", data)
+        .then((response) => {
+          if (response.data.status == 1 && response.data.count == 1) {
+            this.reloadStore();
+            this.formModalToggle();
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+    deleteItem(item, index) {
+      this.axios
+        .delete(
+          "http://localhost:8080/CrmOferteAntent/Delete/" + item.IdOfertaAntent
+        )
+        .then((response) => {
+          if (response.data.status == 1 && response.data.count == 1)
+            this.dataSource.splice(index, 1);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+};
+</script>
--- src/pages/Offers.vue
+++ src/pages/Offers.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 bg-light p-5">
-        <h1>Offers</h1>
+  <template v-if="isLoading == true">
+    <loading />
+  </template>
+  <template v-else>
+    <div class="col-12 p-3"><h1>Offers</h1></div>
+
+    <div class="col-12 col-md-10 col-lg-8 py-3 bg-light">
+      <div class="table-responsive">
+        <crm-offers-header-table />
       </div>
     </div>
-  </div>
+  </template>
 </template>
+<script>
+import Loading from "@/components/Loading";
+import CrmOffersHeaderTable from "../components/Crm/CrmOffers/CrmOffersHeaderTable.vue";
+
+export default {
+  name: "offers",
+  components: {
+    Loading,
+    CrmOffersHeaderTable,
+  },
+};
+</script>
