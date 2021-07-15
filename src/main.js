21b1ec7d887f6017b9042a36e37fb15b2005c8cb
dpricop <pricopdumitru94@gmail.com>
2021-07-15 03:10:57 +0300

    Main.JS - utils has been added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import useVuelidate from "@vuelidate/core";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "bootstrap/dist/js/bootstrap.min";
+import bootstrap from "bootstrap/dist/js/bootstrap.min";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-createApp(App)
+const app = createApp(App);
+
+app
   .use(store)
   .use(router)
+  .use(useVuelidate)
   .use(VueAxios, axios)
   .mount("#app");
+
+app.config.globalProperties.$filters = {
+  dateTimeFormat(value) {
+    if (value == undefined) return " ";
+
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: false,
+    };
+    return new Intl.DateTimeFormat("ro-RO", options).format(new Date(value));
+  },
+};
+window.bootstrap = bootstrap;
